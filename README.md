e0e00a443fba21bd507bc0992980b515ff7fbace
Juanan <juanantonioamillopez@gmail.com>
2024-02-12 09:44:35 +0100

    Inicio del curso de NodeJS. README de presentación y primeros pasos

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# 🟩 Curso de NodeJS 🟩
+
+Vamos a realizar el curso de NodeJS de MiduDEV
+
+
+## Links
+
+Aquí aparecerá el [link](https://youtu.be/yB4n_K7dZV8?si=gcMoZh3zigNYQPrK) gracias al cual haremos este curso. 
---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Inicio del curso de NodeJS: Con El objeto console y algunos de sus métodos, haremos un 'Hola Mundo'. Utilizaremos
+ * .log(), .info(), .error()... los que queramos.
+ */
+
+console.log('Hola Node');
+console.info('Hola Info');
+console.error('Hola Error');
