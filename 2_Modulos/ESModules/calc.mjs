343703ed6a9f3e5a54593e5b5d49030d25942a17
Juanan <juanantonioamillopez@gmail.com>
2024-02-13 11:19:21 +0100

    ESModules

---
+++ 2_Modulos/ESModules/calc.mjs
@@ -0,0 +1,15 @@
+export function suma(num1, num2) {
+  return num1 + num2;
+}
+
+export function resta(num1, num2) {
+  return num1 - num2;
+}
+
+export function multi(num1, num2) {
+  return num1 * num2;
+}
+
+export function divi(num1, num2) {
+  return num1 / num2;
+}
---
+++ 2_Modulos/ESModules/index.mjs
@@ -0,0 +1,15 @@
+/**
+ * Este fichero tiene la extensión .mjs para utilizar ECMAScript Modules. Actualmente, es la forma preferida para
+ * trabajar con NodeJS ya que, CommonJS está prácticamente obsoleto o DEPRECADO. A continuación explicamos las distintas
+ * extensiones que se pueden utilizar:
+ * .js --> utiliza por defecto CommonJS
+ * .mjs --> forzar el uso de ES Modules
+ * .cjs --> forzar el uso de CommonJS
+ */
+
+import { suma, resta, multi, divi } from './calc.mjs';
+
+console.log('SUM: ', suma(4, 5));
+console.log('RES: ', resta(4, 5));
+console.log('MUL: ', multi(4, 5));
+console.log('DIV: ', divi(4, 5));
